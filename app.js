329f9731c6cb4c549ebcd5293faa60d8c0ba1ec0
李雲 <liyun@riunnoMacBook-Pro.local>
2024-06-13 00:12:35 +0900

    add api test

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var catRouter = require('./routes/cat');
+var dogRouter = require('./routes/dog');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/cat', catRouter);
+app.use('/dog', catRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
